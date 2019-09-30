d1688ae407f8787de9a74aca833122f4d16cdf91
Nikolai Shkrabkov <grr_by@mail.ru>
2019-09-30 13:11:56 +0300

    feat: add code of function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function multiply(first, second) {
-  // your solution
+	let result = new Array();
+	resultLength = first.length + second.length;
+	while (resultLength > 0) {
+		result.push(0);
+		resultLength--;
+	}
+	first = first.split('').reverse();
+	second = second.split('').reverse();
+	first.forEach((item, index) => {abc(item, index)});
+	function abc(item, index) {
+		let denary = 0;
+		second.forEach((item2, index2) => {
+			result[result.length - index - index2 - 1] += denary + item * item2;
+			denary = Math.floor(result[result.length - index - index2 - 1] / 10);
+			result[result.length - index - index2 - 1] %= 10;
+		});
+		result[first.length - index - 1] = denary;
+	}
+	return result = result.join('').replace(/^0/, '');
 }
